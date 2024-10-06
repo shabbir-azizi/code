e219fe1f4ebcf734111b8bf9b36435da3cc329cd
unknown <iltfhuss@gmail.com>
2024-10-06 20:41:11 +0500

    Initial commit

---
+++ abc.py
@@ -0,0 +1,36 @@
+Name="shabbir"
+age = 19
+hight=6.1
+
+
+age2=age
+
+Name21=Name
+
+
+print("my name is :",Name)
+
+print("my age is :",age)
+
+print("my hight is :",hight)
+
+print("my name is :",Name21)
+
+print("my age is :",age2)
+
+
+print(Name21)
+print(age2)
+
+
+
+
+a=9
+b=5
+sub=a+b
+print(sub)
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
