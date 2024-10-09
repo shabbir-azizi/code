99fc2c386df5214de979d231d4a8fa29ff8acac0
unknown <iltfhuss@gmail.com>
2024-10-09 21:26:11 +0500

    basics python code

---
+++ forurth.py
@@ -0,0 +1,8 @@
+# str1 ="this is a string"
+# str2 ="apna gali"
+# str3 ="""this is a string"""
+# 'this is apna gali" s tutorial'
+# print ( str1 )
+# print(str3)
+str4 ="hi my name is shabbir hussain and i  am 20 years old"
+print(str4)
