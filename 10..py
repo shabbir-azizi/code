400728c1a8a87d3377c55367bff668d103a051a6
unknown <iltfhuss@gmail.com>
2024-10-14 23:22:07 +0500

    added code

---
+++ 10..py
@@ -0,0 +1,29 @@
+# marks = int (input("enter student marks : "))
+
+# if (marks >= 90):
+#     grade="A"
+# elif(marks >= 80 and marks < 90):
+#     grade="B"
+# elif(marks >= 70 and marks < 80):
+#     grade="C"
+# else:
+#     grade="D"
+
+#     print("grade of the student ->",grade)
+
+
+ 
+# age = input("enter age: ")
+
+# if (age >= 23):
+#     if(age>=80):
+#         print("cannot drive")
+#     else:
+#          print("can drivr")
+
+# else:
+#     print("cannot drive")
+
+
+
+
