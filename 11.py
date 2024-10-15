9188eb444bde5fd71c45f5c43de0da506820715f
unknown <iltfhuss@gmail.com>
2024-10-15 23:15:42 +0500

    added code

---
+++ 11.py
@@ -0,0 +1,28 @@
+# list = [2, 1, 3]
+# print(list.sort())
+# print(list)
+
+# list= ("banana " "litchi" "apple")
+# list= ['a ','b','c','d','e','h')
+
+# print(list.append(4))
+# print(list.sort())
+# print(list)
+
+
+
+# list= ['a','b','c','d','e','h']
+
+# list.reverse()
+# print(list)
+
+# list= [2, 1, 3]
+# list.insert(1, 5)
+# print(list)
+# list =[2, 1, 3, 1]
+# list.pop(2)
+# print(list)
+
+# tup =(2, 1, 3, 1) 
+# print(tup(0))
+# print(tup(1))
