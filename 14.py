a7afbd2fdf7aba32deee4bd21348d6decfdfdc68
unknown <iltfhuss@gmail.com>
2024-10-18 19:14:16 +0500

    basics

---
+++ 14.py
@@ -0,0 +1,44 @@
+# str2 ="hi nmy name is shabbir hussain \n how are you"
+# str1="hi my name  is sabbir"
+
+# str2="i am 19 years old"
+
+
+# print(str2)
+# print( str1+str2)
+# str1="hi how are you"
+# len1=len(str1)
+# print(len1)
+
+# str2="\nnice to meet you"
+# len2=len(str2)
+# print(len2)
+
+# final_str = str1 +"\nthis  is my book "+ str2
+
+# print ( final_str)
+# print(len(final_str))
+
+# num = int(input("enter number"))
+
+# if(num % 2 == 0):
+    # print("even")
+# else:
+    # print("ood")
+
+a = int(input("enter frist number"))
+
+b = int(input("enter second number"))
+
+c = int(input("enter third number"))
+
+if(a >= b and a>= c ):
+    print ("frist number is largest",a)
+
+elif(b >= c):
+    print("second number is largest",b)
+
+else:
+    print("third number is largest",c)
+
+
