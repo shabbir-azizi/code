5bed955ba20288819f57cde2d1b0adfa3112bd29
shabbir-azizi <azizishabbir355@gmail.com>
2024-10-19 23:54:38 +0500

    basics

---
+++ 15.py
@@ -0,0 +1,34 @@
+
+# a = int(input("enter frist number"))
+
+# b = int(input("enter second number"))
+
+# c = int(input("enter third number"))
+
+# if(a >= b and a>= c ):
+#     print ("frist number is largest",a)
+
+# elif(b >= c):
+#     print("second number is largest",b)
+
+# else:
+#     print("third number is largest",c)
+
+# frist = int(input("enter frist : " ))
+# second = int (input("enter second :"))
+
+# print("sum =", frist + second )
+
+# side = float(input("enter square side :"))
+# print("area =", side**2)
+
+# a = float(input("enter frist :"))
+# b = float(input("enter second :"))
+# print("avg =",(a+b)/2)
+
+b = int(input("enter second :"))
+
+b = int(input("enter second :"))
+
+print(a>=b)
+
