4747aef44228c8e947f5fc022a8129cbb6eff493
Shabbir <azizishabbir355@gmail.com>
2024-10-25 22:57:11 +0500

    added basics code

---
+++ shabbir.py
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+
+# str="i am shabbbir from pakistan"
+# print(str.find("z"))
+# str="i am shabbir from pakistam and i am learning python"
+# print(str.count("from"))
+# print(str.count("am"))
+# print(str.count("i"))
+# age =input("enter your age ")
+# print("enter your age",(age) )
+
+# name =input("enter your name")17
+# print("legth of your name is ",len(name))
+# str="my name is shabbie my father name is abc"
+# print(str.count("my"))
+
+
+
+# # age=21
+# age=21
+# if(age >= 18):
+#     # print("can vot & aply for licanse")
+#     print("can drive")
+#     print("can vote")
+
+
+
+
+
+
+
+
+
+
+
+# a = 2 
+# b = 4.25
+# sum = a + b
+
+# print(sum)
+
+# a = int("2")
+
+# a = float("2")
+# b = 4.25
+# sum = a + b
+# print(type(a))
+# print(sum)
+
+# # a = 4.25
+# # a = str(a)/
+
+# # print(type(a))
+
+
+
+# # name = input("enter your name:")
+
+# # print("hi how are you ", name)
+
+# name = input("enter name")
+# age = int(input("enter age"))
+# marks = float(input("enter marks"))
+
+# print("welcome",name)
+# print("age =",age)
+# print("tnku bandal bandal=",marks)
+  
+
+
+
+
+
+
+def printboard():
+    print(f"0 | 1 | 2 |")
+    print(f"---|---|---|")
+    print(f"3 | 4 | 5 |")
+    print(f"---|---|---|")
+    print(f"6 | 7 | 8 |")
+    
+if __name__ == "__main__":
+    xstate = [0, 0, 0, 0, 0, 0, 0, 0]
+    zstate = [0, 0, 0, 0, 0, 0, 0, 0]
+    print("welcome to tic tac toe")
+    print("x's chance")
+    printboard()
