610fc8a4e21cf834431bdb6e37eb05ec0ab23568
Shabbir <azizishabbir355@gmail.com>
2024-11-22 23:20:16 +0500

    basics

--- all/all.py
+++ all/all.py
@@ -111,3 +111,45 @@ print("List after replacing an element:", numbers)
 # Slice the list
 sliced_list = numbers[1:4]  # Get elements from index 1 to 3
 print("Sliced List (index 1 to 3):", sliced_list)
+
+def task():
+
+   tasks =[]#empty list
+   print("----WELCOME TO THE TASK MANNAGEMENT APP----")
+
+   total_task =int(input("Enter how many tast you want to add="))#5
+   for i in range (1, total_task+1):
+        task_name =input(f"enter task{i} = ")#enter task 3 =
+        tasks.append(task_name)
+
+
+print(f"today's task are\n{tasks}")
+    
+while True:
+            operation = int(input("enter 1-add\nd-update\n3-delete\n4-view\n5-exit/stop/"))
+            if operation ==1:
+                 add = input("enter task you want to add = ")
+                 task.append(add)
+                 print(f"task {add} has been successfull added...")
+            elif operation == 2:
+                 updated_val = input ("enter the task name you want to update =")
+                 if updated_val in tasks:
+                      up = input("enter new task =")
+                      ind = task.index(updated_val)#2
+                      tasks [ind]=up
+                      print(f"updated task {up}")
+            elif operation == 3:
+                 del_val =input("which task you want to delete")
+                 if del_val in tasks:
+                      ind = tasks.index (del_val)
+                      del tasks[ind]
+                      print(f"task{del_val} has been deleted...")
+            elif operation ==4:
+                 print(f"total tasks = {tasks}")
+
+            elif operation == 5:
+                 print("closing the program....")
+                 break
+            
+            else: 
+                 print("invalid input")
