b1f6ce86e90d0c189c21648c87b017e2e50d7643
Shabbir <azizishabbir355@gmail.com>
2024-12-01 17:18:41 +0500

    basics

--- all/speed.py
+++ all/speed.py
@@ -1,4 +1,5 @@
 from logging import root
+from pydoc import text
 import tkinter as tk
 
 root = tk.Tk()
@@ -6,8 +7,19 @@ root = tk.Tk()
 label1 = tk.Label(text='welcome to ss coder guy')
 label1.grid(row =0,column=0)
 
+text
+# relief
+# height
+# width
+# bd
+# bg
+# fg
+
 
 root.mainloop()
 
 
+import tkinter as tk
+window = tk.Tk()
+window.mainloop()
 
