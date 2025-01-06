42e1a5512b1416c03f12b05361a103fb088fee9f
Shabbir <azizishabbir355@gmail.com>
2025-01-06 20:46:37 +0500

    basics

---
+++ all/speed tyoing 1.py
@@ -0,0 +1,17 @@
+# Thise is my text. plase tyoe it fast.
+# Thise is my text.please type is fast
+
+
+from time import *
+import random as r
+
+
+test=["A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long"
+"my name is shabbir quetta","welcome to wscube tech jodhpur"]
+test1 =r.choice(test)
+print("***** tyoing speed *****")
+print(test1)
+
+
+
+
