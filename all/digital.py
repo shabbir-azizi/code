2ec07d696798a1358a97a092a09b167eb93d3472
Shabbir <azizishabbir355@gmail.com>
2025-01-11 22:38:32 +0500

    basics

---
+++ all/digital.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+clock = Tk()
+clock.tital('    **** Digital Clock ****')
+clock.geometry('1000*500')
+clock.config(bg='yellow')
+
+
+
+lab_hr=label (clock,text="00",font=('time new roman',60,"bold")
+              
+                                    bg='red' ,fg'="white")
+lab_hr.Place(*=40,y=40,height=110,width=100)
+                                                                                                                  ))
+
+
+
+
+
+clock.mainloop()
