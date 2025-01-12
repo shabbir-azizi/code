c80779896a101a93bf2d507f0934ee22cfae87a2
Shabbir <azizishabbir355@gmail.com>
2025-01-12 18:13:33 +0500

    basics

--- all/digital.py
+++ all/digital.py
@@ -1,20 +1,22 @@
 from tkinter import *
 
 clock = Tk()
-clock.tital('    **** Digital Clock ****')
-clock.geometry('1000*500')
+clock.title('**** Digital Clock ****')
+clock.geometry('1000x500')
 clock.config(bg='yellow')
 
+lab_hr = Label(clock, text="00", font=('times new roman', 60, "bold"),
+               bg='red', fg="white")
+lab_hr.place(x=120, y=40, height=110, width=100)
+lab_min = Label(clock, text="00", font=('times new roman', 60, "bold"),
+               bg='red', fg="white")
+lab_min.place(x=340, y=40, height=110, width=100)
+lab_sec = Label(clock, text="00", font=('times new roman', 60, "bold"),
+               bg='red', fg="white")
+lab_sec.place(x=560, y=40, height=110, width=100)
 
-
-lab_hr=label (clock,text="00",font=('time new roman',60,"bold")
-              
-                                    bg='red' ,fg'="white")
-lab_hr.Place(*=40,y=40,height=110,width=100)
-                                                                                                                  ))
-
-
-
-
+lab_am = Label(clock, text="00", font=('times new roman', 60, "bold"),
+               bg='red', fg="white")
+lab_am .place(x=780, y=40, height=110, width=100)
 
 clock.mainloop()
--- all/speed.py
+++ all/speed.py
@@ -63,3 +63,4 @@ window.mainloop()
 
 
 
+
