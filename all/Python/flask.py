3344ece1d25f0ee401bc9a71fd8df80403a3b0f7
Shabbir <azizishabbir355@gmail.com>
2025-02-07 23:35:35 +0500

    basics python

---
+++ all/Python/flask.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template_string, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        name = request.form.get("name")
+        if name:
+            return f"<h1>Hello, {name}!</h1>"
+        return "<h1>Please enter your name.</h1>"
+    return '''
+        <form method="post">
+            <label for="name">Enter your name:</label>
+            <input type="text" id="name" name="name">
+            <button type="submit">Submit</button>
+        </form>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
