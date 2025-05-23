d095f272915c217e64abb2d79eac9fa750383c6b
Shabbir <azizishabbir355@gmail.com>
2025-05-23 15:07:28 +0500

    work on c sharp

--- all/c language/practice.c
+++ all/c language/practice.c
@@ -1,18 +1,1 @@
 include <iostream>
-
-
-int main() {
-    printf("hellow pw");
-    printf(\n)
-    printf("hellow lw");
-    return 0;
-    float x = 5
-    float c = 9
-    float r = 3
-    float z = 5 / 2.5;'';
-    printf("%f",z);
-
-    int r = 5;
-    int v = 4 * 3.14 * r * r * r / 3;
-    printf("the volume is : %f",v);
-    }
--- gghk.py
+++ gghk.py
@@ -60,3 +60,16 @@ print("Basic Python concepts covered!")
 
 
 
+               bg='gray', fg="black")
+lab_year_txt.place(x=560, y=410, height=40, width=110)
+
+lab_day = Label(clock, text="00", font=('times new roman', 35, "bold"),
+               bg='gray', fg="black")
+lab_day.place(x=780, y=270, height=110, width=100)
+
+lab_day_txt = Label(clock, text="Day", font=('times new roman', 20, "bold"),
+               bg='gray', fg="black")
+lab_day_txt.place(x=780, y=410, height=40, width=100)
+
+date_time()
+clock.mainloop()
